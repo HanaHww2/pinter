68878b9e0e9948cf0eac9747c94a30f91b2f6068
HanaHww2 <hanahww2@gmail.com>
2021-01-14 17:18:07 +0900

    adjusted form and adding WYSIWYG editor

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,16 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreateForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable',
+                                                           'style':'min-height: 10rem; heigth:auto;'+
+                                                                   'text-align:left;'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['project', 'title', 'image', 'content']
